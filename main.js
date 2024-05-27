51efa0f48faf7eb342da4dcb0d2191e4e524d820
saanidhy <102911624+saanidhy@users.noreply.github.com>
2024-05-27 15:04:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+var first_name="Ranbir ";
+var last_name="Kapoor";
+var full_name=first_name.concat(last_name);
+console.log(full_name);
+var a=10;
+var b=5;
+var add=a+b;
+var sub=a-b;
+var into=a*b;
+var div=a/b;
+console.log(add);
+console.log(sub);
+console.log(into);
+console.log(div);
